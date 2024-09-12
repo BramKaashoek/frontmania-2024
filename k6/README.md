9de09804645ac12db4da9d8db575a675b673939c
Bram Kaashoek <bramkaashoek@gmail.com>
2024-09-12 09:02:13 +0200

    initial commit

---
+++ k6/README.md
@@ -0,0 +1,2 @@
+`pnpm i -g k6`
+`k6 run load.js`
---
+++ k6/load.js
@@ -0,0 +1,37 @@
+import { randomItem } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+import { check, group } from 'k6';
+import http from 'k6/http';
+
+const PAGES = ['/'];
+const TARGET_RATE = 60;
+
+export const options = {
+  scenarios: {
+    pages: {
+      executor: 'ramping-arrival-rate',
+      startRate: 1,
+      timeUnit: '1s',
+      preAllocatedVUs: 100,
+      stages: [
+        { target: TARGET_RATE, duration: '1s' },
+        { target: TARGET_RATE, duration: '45m' },
+        { target: 1, duration: '3m' },
+      ],
+    },
+  },
+};
+
+export default function (data) {
+  group('Load', function () {
+    const page = randomItem(PAGES);
+
+    const res = http.get(`http://localhost:3000${page}`, {
+      headers: {
+        'Cache-Control': 'no-cache',
+        'accept-encoding': 'gzip',
+      },
+    });
+
+    check(res, { 'status was 200': (r) => r.status === 200 });
+  });
+}
